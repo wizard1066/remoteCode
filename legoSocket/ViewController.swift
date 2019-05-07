0a71e11aaabf6ad64e2f7cb36fa077817fb723bb
Mark Lucking <mark.lucking@gmail.com>
2019-05-07 09:49:26 +0200

    Initial Commit

---
+++ legoSocket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  legoSocket
+//
+//  Created by localadmin on 07.05.19.
+//  Copyright © 2019 ch.cqd.legoblue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
