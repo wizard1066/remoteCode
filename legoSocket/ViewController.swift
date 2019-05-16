23236a363c7577804784e8402da09291deafdf8d
Mark Lucking <mark.lucking@gmail.com>
2019-05-16 17:48:29 +0200

    Alpha version 1.2 Sorted out short+long press on buttons, added some error messages when connections lost

--- legoSocket/ViewController.swift
+++ legoSocket/ViewController.swift
@@ -38,7 +38,51 @@
 
 import UIKit
 
-class ViewController: UIViewController, UpdateDisplayDelegate {
+class ViewController: UIViewController, UpdateDisplayDelegate, FeedBackConnection, ChangeTag {
+
+  func newName(_ value: String) {
+    let blob = value.components(separatedBy: ":")
+    switch blob[1] {
+      case "north":
+        northButton.titleLabel?.text = blob[2]
+        break
+      case "south":
+        southButton.titleLabel?.text = blob[2]
+        break
+      case "east":
+        eastButton.titleLabel?.text = blob[2]
+        break
+      case "west":
+        westButton.titleLabel?.text = blob[2]
+        break
+      case "central":
+        centralButton.titleLabel?.text = blob[2]
+        break
+      case "northeast":
+        northEastButton.titleLabel?.text = blob[2]
+        break
+      case "northwest":
+        northWestButton.titleLabel?.text = blob[2]
+        break
+      case "southeast":
+        southEastButton.titleLabel?.text = blob[2]
+        break
+      case "southwest":
+        southWestButton.titleLabel?.text = blob[2]
+        break
+      default:
+        ok("label syntax label:central:blah")
+    }
+  }
+
+  func ok(_ value: String) {
+    let alertController = UIAlertController(title: value, message: value, preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+
+    self.present(alertController, animated: true, completion: nil)
+  }
+  
   
   func port(_ value:String) {
     if value.contains("port1") {
@@ -78,16 +122,146 @@ class ViewController: UIViewController, UpdateDisplayDelegate {
   @IBOutlet weak var portA: UILabel!
   
   
-  @IBAction func up(_ sender: UIButton) {
+  @IBOutlet weak var northButton: UIButton!
+  @IBOutlet weak var northEastButton: UIButton!
+  @IBOutlet weak var eastButton: UIButton!
+  @IBOutlet weak var southEastButton: UIButton!
+  @IBOutlet weak var southButton: UIButton!
+  @IBOutlet weak var southWestButton: UIButton!
+  @IBOutlet weak var westButton: UIButton!
+  @IBOutlet weak var northWestButton: UIButton!
+  @IBOutlet weak var centralButton: UIButton!
+  
+  class MyTapGesture: UITapGestureRecognizer {
+    var tag:String?
+  }
+  
+  class MyLongGesture: UILongPressGestureRecognizer {
+    var tag:String?
+  }
+  
+  @objc func openShortCall(sender : MyTapGesture) {
+    let tag = sender.tag
+    print("short:",tag!)
+    chatRoom.sendMessage(message: String(tag!))
+  }
+  
+  @objc func openLongCall(sender : MyLongGesture) {
+    let tag = sender.tag
+    if(sender.state == UIGestureRecognizer.State.began) {
+      print("long:",tag!)
+      chatRoom.sendMessage(message: String(tag!))
+    }
+  }
+  
+  func configButtons() {
+    let buttonCorner:CGFloat = 32
+    
+    let oneDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let oneDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    oneDigitShort.tag = "1S"
+    oneDigitLong.tag = "1L"
+    oneDigitShort.numberOfTapsRequired = 1
+    northWestButton.layer.cornerRadius = buttonCorner
+    northWestButton.addGestureRecognizer(oneDigitShort)
+    northWestButton.addGestureRecognizer(oneDigitLong)
+    
+    let twoDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let twoDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    twoDigitShort.tag = "2S"
+    twoDigitLong.tag = "2L"
+    twoDigitShort.numberOfTapsRequired = 1
+    northButton.layer.cornerRadius = buttonCorner
+    northButton.addGestureRecognizer(twoDigitShort)
+    northButton.addGestureRecognizer(twoDigitLong)
+    
+    let threeDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let threeDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    threeDigitShort.tag = "3S"
+    threeDigitLong.tag = "3L"
+    threeDigitShort.numberOfTapsRequired = 1
+    northEastButton.layer.cornerRadius = buttonCorner
+    northEastButton.addGestureRecognizer(threeDigitShort)
+    northEastButton.addGestureRecognizer(threeDigitLong)
+    
+    
+    let fourDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let fourDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    fourDigitShort.tag = "4S"
+    fourDigitLong.tag = "4L"
+    fourDigitShort.numberOfTapsRequired = 1
+    westButton.layer.cornerRadius = buttonCorner
+    westButton.addGestureRecognizer(fourDigitShort)
+    westButton.addGestureRecognizer(fourDigitLong)
+    
+    let fiveDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let fiveDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    fiveDigitShort.tag = "5S"
+    fiveDigitLong.tag = "5L"
+    fiveDigitShort.numberOfTapsRequired = 1
+    centralButton.layer.cornerRadius = buttonCorner
+    centralButton.addGestureRecognizer(fiveDigitShort)
+    centralButton.addGestureRecognizer(fiveDigitLong)
+
+    let sixDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let sixDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    sixDigitShort.tag = "6S"
+    sixDigitLong.tag = "6L"
+    sixDigitShort.numberOfTapsRequired = 1
+    eastButton.layer.cornerRadius = buttonCorner
+    eastButton.addGestureRecognizer(sixDigitShort)
+    eastButton.addGestureRecognizer(sixDigitLong)
+    
+    
+    let sevenDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let sevenDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    sevenDigitShort.tag = "7S"
+    sevenDigitLong.tag = "7L"
+    sevenDigitShort.numberOfTapsRequired = 1
+    southWestButton.layer.cornerRadius = buttonCorner
+    southWestButton.addGestureRecognizer(sevenDigitShort)
+    southWestButton.addGestureRecognizer(sevenDigitLong)
+    
+    
+    let eightDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let eightDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    eightDigitShort.tag = "8S"
+    eightDigitLong.tag = "8L"
+    eightDigitShort.numberOfTapsRequired = 1
+    southButton.layer.cornerRadius = buttonCorner
+    southButton.addGestureRecognizer(eightDigitShort)
+    southButton.addGestureRecognizer(eightDigitLong)
+    
+    
+    let nineDigitShort = MyTapGesture(target: self, action: #selector(self.openShortCall))
+    let nineDigitLong = MyLongGesture(target: self, action: #selector(self.openLongCall))
+    nineDigitShort.tag = "9S"
+    nineDigitLong.tag = "9L"
+    nineDigitShort.numberOfTapsRequired = 1
+    southEastButton.layer.cornerRadius = buttonCorner
+    southEastButton.addGestureRecognizer(nineDigitShort)
+    southEastButton.addGestureRecognizer(nineDigitLong)
+    
+
+  }
+  
+  @IBAction func north(_ sender: UIButton) {
     chatRoom.sendMessage(message: "north\n")
+    print("north Action")
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector (tap))
+    let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(long))
+    tapGesture.numberOfTapsRequired = 1
+    northButton.addGestureRecognizer(tapGesture)
+    northButton.addGestureRecognizer(longGesture)
+    
   }
-  @IBAction func down(_ sender: UIButton) {
-    chatRoom.sendMessage(message: "south\n")
-  }
-  @IBAction func right(_ sender: UIButton) {
+//  @IBAction func south(_ sender: UIButton) {
+//    chatRoom.sendMessage(message: "south\n")
+//  }
+  @IBAction func east(_ sender: UIButton) {
     chatRoom.sendMessage(message: "east\n")
   }
-  @IBAction func left(_ sender: UIButton) {
+  @IBAction func west(_ sender: UIButton) {
     chatRoom.sendMessage(message: "west\n")
   }
 
@@ -106,9 +280,28 @@ class ViewController: UIViewController, UpdateDisplayDelegate {
   @IBAction func southWest(_ sender: UIButton) {
     chatRoom.sendMessage(message: "southWest\n")
   }
+  
+  @IBAction func central(_ sender: UIButton) {
+    chatRoom.sendMessage(message: "central\n")
+  }
+  
+  @objc func tap(sender:UITapGestureRecognizer) {
+    let pressed = "S" + String(sender.view?.tag ?? 0) + "\n"
+    chatRoom.sendMessage(message: pressed)
+  }
+
+  @objc func long(sender: UILongPressGestureRecognizer) {
+    let pressed = "L" + String(sender.view?.tag ?? 0) + "\n"
+    chatRoom.sendMessage(message: pressed)
+  }
+  
   override func viewDidLoad() {
+
     super.viewDidLoad()
     chatRoom.delegate = self
+    chatRoom.connection = self
+    chatRoom.rename = self
+    configButtons()
   }
 
 }
