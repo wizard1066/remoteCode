996bc9f34679f68f0f45b614100b7a4d0f4950d3
Mark Lucking <mark.lucking@gmail.com>
2019-05-27 11:47:55 +0200

    Few minor fixes relating to port names and labels

--- robotRemote/AnalogVC.swift
+++ robotRemote/AnalogVC.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, ChangeTag, UIGestureRecognizerDelegate {
-
+  
   func newName(_ value: String) {
-     let blob = value.components(separatedBy: ":")
+    let blob = value.components(separatedBy: ":")
     // Change the labels on the buttons
-
+    
     if blob.count <= 2 {
       return // corrupted data
     }
-
+    
     // Change the labels of the ports
     for ports in xPort {
       if blob[1] == ports.key {
@@ -35,36 +35,36 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
   func ok(_ value: String) {
     let alertController = UIAlertController(title: value, message: value, preferredStyle: .alert)
     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(defaultAction)
+    alertController.addAction(defaultAction)
     self.present(alertController, animated: true, completion: nil)
   }
   
   func bad(_ value: String) {
     let alertController = UIAlertController(title: value, message: value, preferredStyle: .alert)
-      let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-      alertController.addAction(defaultAction)
-      self.present(alertController, animated: true, completion: {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 8.0, execute: {
-          chatRoom.stopChat()
-          self.performSegue(withIdentifier: "returnToSegue", sender: self)
-        })
+    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+    alertController.addAction(defaultAction)
+    self.present(alertController, animated: true, completion: {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 8.0, execute: {
+        chatRoom.stopChat()
+        self.performSegue(withIdentifier: "returnToSegue", sender: self)
       })
+    })
   }
   
-//  func port(_ value: String) {
-//    let blob = value.components(separatedBy: ":")
-//
-//    let portAssign = ["1":port1,"2":port2,"3":port3,"4":port4,"A":portA,"B":portB,"C":portC,"D":portD]
-//    let port1A = String(blob[0].last!)
-//    let port2D = "P" + String(blob[0].last!)
-//    let port2F = portAssign[port1A]
-//    if xPort[port2D] != nil {
-//      let replaced = value.replacingOccurrences(of: port2D, with: xPort[port2D]!)
-//      if port2F!?.text != nil {
-//        port2F!?.text = replaced
-//      }
-//    }
-//  }
+  //  func port(_ value: String) {
+  //    let blob = value.components(separatedBy: ":")
+  //
+  //    let portAssign = ["1":port1,"2":port2,"3":port3,"4":port4,"A":portA,"B":portB,"C":portC,"D":portD]
+  //    let port1A = String(blob[0].last!)
+  //    let port2D = "P" + String(blob[0].last!)
+  //    let port2F = portAssign[port1A]
+  //    if xPort[port2D] != nil {
+  //      let replaced = value.replacingOccurrences(of: port2D, with: xPort[port2D]!)
+  //      if port2F!?.text != nil {
+  //        port2F!?.text = replaced
+  //      }
+  //    }
+  //  }
   
   func port(_ value: String) {
     let blob = value.components(separatedBy: ":")
@@ -94,7 +94,7 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
   }
   
   func configurePorts() {
-  let ports2D = ["1":port1,"2":port2,"3":port3,"4":port4,"A":portA,"B":portB,"C":portC,"D":portD]
+    let ports2D = ["1":port1,"2":port2,"3":port3,"4":port4,"A":portA,"B":portB,"C":portC,"D":portD]
     for portVariable in ports2D {
       let portTapGesture = MyPortTapGesture(target: self, action: #selector(self.openPortTap))
       portTapGesture.port = portVariable.key
@@ -102,7 +102,18 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
       portVariable.value?.addGestureRecognizer(portTapGesture)
       portVariable.value?.isUserInteractionEnabled = true
     }
+  }
+  
+  func confirmPortNames() {
+    let portAssign = ["P1":port1,"P2":port2,"P3":port3,"P4":port4,"PA":portA,"PB":portB,"PC":portC,"PD":portD]
+    if portNames.count != 0 {
+      for ports in portAssign {
+        if portNames[ports.key] != nil {
+          portAssign[ports.key]!?.text = portNames[ports.key]
+        }
+      }
     }
+  }
   
   @IBOutlet weak var topSV: UIStackView!
   @IBOutlet weak var lowSV: UIStackView!
@@ -124,14 +135,14 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
   @IBOutlet weak var touchPad: UIView!
   
   func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
-       shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
-        -> Bool {
-        return true
-      }
+                         shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
+    -> Bool {
+      return true
+  }
   
   @IBAction func unwindAnalogVC(segue: UIStoryboardSegue) {
-      print("AnalogVC")
-    }
+    print("AnalogVC")
+  }
   
   
   override func viewDidLoad() {
@@ -141,7 +152,7 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
     chatRoom.connection = self
     chatRoom.rename = self
     configurePorts()
-
+    
     let edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(screenEdgeSwiped))
     edgePan.edges = .left
     edgePan.delegate = self
@@ -154,28 +165,30 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
   
   @objc func screenEdgeSwiped(_ recognizer: UIScreenEdgePanGestureRecognizer) {
     if recognizer.state == .recognized {
-        
-        self.performSegue(withIdentifier: "returnToSegue", sender: self)
+      
+      self.performSegue(withIdentifier: "returnToSegue", sender: self)
     }
-}
-
-@objc func otherEdgeSwiped(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+  }
+  
+  @objc func otherEdgeSwiped(_ recognizer: UIScreenEdgePanGestureRecognizer) {
     if recognizer.state == .recognized {
-        self.performSegue(withIdentifier: "returnToSegue", sender: self)
+      self.performSegue(withIdentifier: "returnToSegue", sender: self)
     }
-}
+  }
   
   override func viewDidAppear(_ animated: Bool) {
     chatRoom.sendMessage(message: "#:analog")
     analogVC = true
     print("VCs dV \(digitalVC) aV \(analogVC) mV \(motionVC)")
+    print("fuck \(portNames)")
+    confirmPortNames()
   }
   
   var position: CGPoint!
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
     if timer != nil {
-            timer.invalidate()
+      timer.invalidate()
     }
     if let touch = event?.allTouches?.first {
       position = touch.location(in: touchPad)
@@ -184,21 +197,21 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
       } else {
         let (joyX,joyY,figure2S) = calcPoint(cord2D: position)
         if position.x > 128 && position.y < 128 {
-//          legoImage.image = UIImage(named: "topRight")
+          //          legoImage.image = UIImage(named: "topRight")
         }
         if position.x < 128 && position.y < 128 {
-//          legoImage.image = UIImage(named: "topLeft")
+          //          legoImage.image = UIImage(named: "topLeft")
         }
         if position.x < 128 && position.y > 128 {
-//          legoImage.image = UIImage(named: "lowLeft")
+          //          legoImage.image = UIImage(named: "lowLeft")
         }
         if position.x > 128 && position.y > 128 {
-//          legoImage.image = UIImage(named: "lowRight")
+          //          legoImage.image = UIImage(named: "lowRight")
         }
         xFactor.text = String(Int(joyX))
         yFactor.text = String(Int(joyY))
-//         launch this on a timer
-//        self.legoImage.image = UIImage(named: "center")
+        //         launch this on a timer
+        //        self.legoImage.image = UIImage(named: "center")
         
         chatRoom.sendMessage(message: figure2S)
       }
@@ -210,85 +223,85 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
     loop = 4
-//    timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(slowDownAndStop), userInfo: nil, repeats: true)
+    //    timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(slowDownAndStop), userInfo: nil, repeats: true)
     let figure2S = "@:0:0"
     chatRoom.sendMessage(message: figure2S)
-}
-
-@objc func slowDownAndStop() {
-  loop -= 1
+  }
   
-  position.x = position.x + 120
-  position.y = position.y + 120
-  if position.x > position.y {
-//      let loop = Int(Float(position.x).squareRoot())
-//      for _ in 0 ... loop {
-        if position.x < 120 {
-          position.x = round(position.x * 2)
-        } else {
-          position.x = round(position.x / 2)
-        }
+  @objc func slowDownAndStop() {
+    loop -= 1
     
-        if position.y < 120 {
-          position.y = round(position.y * 2)
-        } else {
-          position.y = round(position.y / 2)
-        }
-    
-    
-    
-//        let string2R = "@:\(position.x):\(position.y)"
-        let (_,_,figure2S) = calcPoint(cord2D: position)
+    position.x = position.x + 120
+    position.y = position.y + 120
+    if position.x > position.y {
+      //      let loop = Int(Float(position.x).squareRoot())
+      //      for _ in 0 ... loop {
+      if position.x < 120 {
+        position.x = round(position.x * 2)
+      } else {
+        position.x = round(position.x / 2)
+      }
+      
+      if position.y < 120 {
+        position.y = round(position.y * 2)
+      } else {
+        position.y = round(position.y / 2)
+      }
+      
+      
+      
+      //        let string2R = "@:\(position.x):\(position.y)"
+      let (_,_,figure2S) = calcPoint(cord2D: position)
+      chatRoom.sendMessage(message: figure2S)
+      
+      if loop == 0 {
+        let figure2S = "@:0:0"
+        position.y = 0
+        position.x = 0
         chatRoom.sendMessage(message: figure2S)
-        
-        if loop == 0 {
-          let figure2S = "@:0:0"
-          position.y = 0
-          position.x = 0
-          chatRoom.sendMessage(message: figure2S)
-          if timer != nil {
-            timer.invalidate()
-          }
-//          break
+        if timer != nil {
+          timer.invalidate()
         }
-//      }
+        //          break
+      }
+      //      }
     } else {
-//      let loop = Int(Float(position.y).squareRoot())
-//      for _ in 0 ... loop {
-    
-        if position.x < 120 {
-          position.x = round(position.x * -2)
-        } else {
-          position.x = round(position.x / 2)
-        }
-    
-        if position.y < 120 {
-          position.y = round(position.y * -2)
-        } else {
-          position.y = round(position.y / 2)
-        }
-        print("position \(position.x) \(position.y)")
-//        let string2R = "@:\(position.x):\(position.y)"
-        let (_,_,figure2S) = calcPoint(cord2D: position)
+      //      let loop = Int(Float(position.y).squareRoot())
+      //      for _ in 0 ... loop {
+      
+      if position.x < 120 {
+        position.x = round(position.x * -2)
+      } else {
+        position.x = round(position.x / 2)
+      }
+      
+      if position.y < 120 {
+        position.y = round(position.y * -2)
+      } else {
+        position.y = round(position.y / 2)
+      }
+      print("position \(position.x) \(position.y)")
+      //        let string2R = "@:\(position.x):\(position.y)"
+      let (_,_,figure2S) = calcPoint(cord2D: position)
+      chatRoom.sendMessage(message: figure2S)
+      
+      if loop == 0 {
+        let figure2S = "@:0:0"
+        position.y = 0
+        position.x = 0
         chatRoom.sendMessage(message: figure2S)
-        
-        if loop == 0 {
-          let figure2S = "@:0:0"
-          position.y = 0
-          position.x = 0
-          chatRoom.sendMessage(message: figure2S)
-          if timer != nil {
-            timer.invalidate()
-          }
-//          break
+        if timer != nil {
+          timer.invalidate()
         }
-//      }
+        //          break
+      }
+      //      }
     }
   }
   
   func calcPoint(cord2D: CGPoint) -> (CGFloat, CGFloat, String) {
     // Need to subtract 120 cause the pad is 240 pixels wide
-  
+    
     let xPosition = cord2D.x - 120
     let yPosition = cord2D.y - 120
     
@@ -302,66 +315,66 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
     let string2R = "@:\(joyX):\(joyY)"
     return (joyX,joyY,string2R)
     
-
+    
   }
-
   
-//  override func viewDidLayoutSubviews() {
-//    if appStart {
-//      appStart = false
-//      detectOrientation()
-//    }
-//  }
   
-    var topYaxisSV:NSLayoutConstraint!
-    var topXaxisSV:NSLayoutConstraint!
-    var lowYaxisSV:NSLayoutConstraint!
-    var lowXaxisSV:NSLayoutConstraint!
+  //  override func viewDidLayoutSubviews() {
+  //    if appStart {
+  //      appStart = false
+  //      detectOrientation()
+  //    }
+  //  }
+  
+  var topYaxisSV:NSLayoutConstraint!
+  var topXaxisSV:NSLayoutConstraint!
+  var lowYaxisSV:NSLayoutConstraint!
+  var lowXaxisSV:NSLayoutConstraint!
   
   func detectOrientation() {
     topSV.translatesAutoresizingMaskIntoConstraints = false
     lowSV.translatesAutoresizingMaskIntoConstraints = false
     
     let margins = view.layoutMarginsGuide
-
-    if UIDevice.current.orientation.isLandscape {
-        // do your stuff here for landscape
-        if topYaxisSV != nil {
-          topYaxisSV.isActive = false
-          topXaxisSV.isActive = false
-          lowYaxisSV.isActive = false
-          lowXaxisSV.isActive = false
-        }
-        topYaxisSV = topSV.centerYAnchor.constraint(equalTo: margins.centerYAnchor, constant: 1)
-        topYaxisSV.isActive = true
-        topXaxisSV = topSV.leftAnchor.constraint(equalToSystemSpacingAfter: margins.leftAnchor, multiplier: 1)
-        topXaxisSV.isActive = true
     
-        lowYaxisSV = lowSV.centerYAnchor.constraint(equalTo: margins.centerYAnchor, constant: 1)
-        lowYaxisSV.isActive = true
-        lowXaxisSV = lowSV.rightAnchor.constraint(equalToSystemSpacingAfter: margins.rightAnchor, multiplier: 1)
-        lowXaxisSV.isActive = true
+    if UIDevice.current.orientation.isLandscape {
+      // do your stuff here for landscape
+      if topYaxisSV != nil {
+        topYaxisSV.isActive = false
+        topXaxisSV.isActive = false
+        lowYaxisSV.isActive = false
+        lowXaxisSV.isActive = false
+      }
+      topYaxisSV = topSV.centerYAnchor.constraint(equalTo: margins.centerYAnchor, constant: 1)
+      topYaxisSV.isActive = true
+      topXaxisSV = topSV.leftAnchor.constraint(equalToSystemSpacingAfter: margins.leftAnchor, multiplier: 1)
+      topXaxisSV.isActive = true
+      
+      lowYaxisSV = lowSV.centerYAnchor.constraint(equalTo: margins.centerYAnchor, constant: 1)
+      lowYaxisSV.isActive = true
+      lowXaxisSV = lowSV.rightAnchor.constraint(equalToSystemSpacingAfter: margins.rightAnchor, multiplier: 1)
+      lowXaxisSV.isActive = true
     } else {
       
-        if topYaxisSV != nil {
-          topYaxisSV.isActive = false
-          topXaxisSV.isActive = false
-          lowYaxisSV.isActive = false
-          lowXaxisSV.isActive = false
-        }
-//          topYaxisSV = topSV.topAnchor.constraint(equalTo: margins.topAnchor, constant: 8)
-          topYaxisSV = topSV.bottomAnchor.constraint(equalTo: touchPad.topAnchor, constant: -32)
-          topYaxisSV.isActive = true
-          topXaxisSV = topSV.centerXAnchor.constraint(equalTo: margins.centerXAnchor, constant: 1)
-          topXaxisSV.isActive = true
+      if topYaxisSV != nil {
+        topYaxisSV.isActive = false
+        topXaxisSV.isActive = false
+        lowYaxisSV.isActive = false
+        lowXaxisSV.isActive = false
+      }
+      //          topYaxisSV = topSV.topAnchor.constraint(equalTo: margins.topAnchor, constant: 8)
+      topYaxisSV = topSV.bottomAnchor.constraint(equalTo: touchPad.topAnchor, constant: -32)
+      topYaxisSV.isActive = true
+      topXaxisSV = topSV.centerXAnchor.constraint(equalTo: margins.centerXAnchor, constant: 1)
+      topXaxisSV.isActive = true
       
-//          lowYaxisSV = lowSV.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -8)
-          lowYaxisSV = lowSV.topAnchor.constraint(equalTo: touchPad.bottomAnchor, constant: 32)
-          lowYaxisSV.isActive = true
-          lowXaxisSV = lowSV.centerXAnchor.constraint(equalTo: margins.centerXAnchor, constant: 1)
-          lowXaxisSV.isActive = true
+      //          lowYaxisSV = lowSV.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -8)
+      lowYaxisSV = lowSV.topAnchor.constraint(equalTo: touchPad.bottomAnchor, constant: 32)
+      lowYaxisSV.isActive = true
+      lowXaxisSV = lowSV.centerXAnchor.constraint(equalTo: margins.centerXAnchor, constant: 1)
+      lowXaxisSV.isActive = true
     }
-}
+  }
   
   override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
     detectOrientation()
@@ -384,18 +397,18 @@ class AnalogVC: UIViewController, UpdateDisplayDelegate, FeedBackConnection, Cha
 }
 
 extension UIPanGestureRecognizer {
-
-    func isLeft(theViewYouArePassing: UIView) -> Bool {
-        let detectionLimit: CGFloat = 50
-        var vel : CGPoint = velocity(in: theViewYouArePassing)
-        if vel.x > detectionLimit {
-            print("Gesture went right")
-            return false
-        } else if vel.x < -detectionLimit {
-            print("Gesture went left")
-            return true
-        }
-        return true
+  
+  func isLeft(theViewYouArePassing: UIView) -> Bool {
+    let detectionLimit: CGFloat = 50
+    var vel : CGPoint = velocity(in: theViewYouArePassing)
+    if vel.x > detectionLimit {
+      print("Gesture went right")
+      return false
+    } else if vel.x < -detectionLimit {
+      print("Gesture went left")
+      return true
     }
+    return true
+  }
   
 }
